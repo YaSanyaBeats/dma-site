9263b4a82cfab00650f265f616c7ebf2e3378704
volokhinn <csgomx@mail.ru>
2023-02-03 14:50:48 +0700

    Added Target service page

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,5 +1,7 @@
 import "%modules%/header/header";
 
+/* Home */
+
 import "%modules%/home/hero/hero";
 import "%modules%/home/quotes/quotes";
 import "%modules%/home/quiz/quiz";
@@ -8,9 +10,23 @@ import "%modules%/home/team/team";
 import "%modules%/home/about/about";
 import "%modules%/home/blog/blog";
 
+/* Breadcrumbs */
+
 import "%modules%/breadcrumbs/breadcrumbs";
+
+/* Blog */
+
 import "%modules%/blog/posts/posts";
 import "%modules%/blog/single-post/single-post";
+
+/* Cases */
+
 import "%modules%/cases/cases";
+
+/* Services */
+
+import "%modules%/services/services";
+
+/* Footer */
 
 import "%modules%/footer/footer";
