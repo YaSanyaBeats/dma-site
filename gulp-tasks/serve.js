916abc72f0f1d963e42ce388e221d9337b754c44
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2023-02-11 13:44:26 +0700

    Fix BrowserSync external ip

--- gulp-tasks/serve.js
+++ gulp-tasks/serve.js
@@ -3,10 +3,12 @@
 import { paths } from "../gulpfile.babel";
 import gulp from "gulp";
 import browsersync from "browser-sync";
+const devip = require('dev-ip');
 
 gulp.task("serve", () => {
     browsersync.init({
         server: "./dist/",
+        host: devip(),
         port: 4000,
         notify: true
     });
